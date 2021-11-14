60d4e3f51d13d67e92592165ccca9b68894a4c45
esra-saatci <esra.saatci86@gmail.com>
2021-11-13 21:42:23 -0500

    Build buttons & apply CSS conditionally

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,24 @@ import React from "react";
 
 import "components/Button.scss";
 
-export default function Button(props) {
-   return <></>;
-}
+ export default function Button(props) {
+   let buttonClass = "button";
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+ 
+   if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
