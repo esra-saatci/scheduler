476182717a96fc96bc99b56259a678000a62456a
esra-saatci <esra.saatci86@gmail.com>
2021-11-17 21:05:46 -0500

    Refactor InterviewerList & InterviewerListItem components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,15 +7,14 @@ export default function InterviewerListItem(props) {
     'interviewers__item--selected': props.selected
   });
 
-  const name = props.name
   return (
-    <li onClick ={() => props.setInterviewer(props.name)} className={interviewerClass}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
-        alt={name}
+        alt={props.name}
       />
-      {props.selected && (name)}
+      {props.selected && props.name}
     </li>
   );
 }
